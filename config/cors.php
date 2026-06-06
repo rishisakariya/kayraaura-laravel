301b4595b8237b2c9755a70dc7faf941c54ef9e1
Rishi Sakariya <rishi.sakriya@gmail.com>
2026-06-06 13:19:52 +0530

    cros

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','http://localhost:3001'],
-
+    // 'allowed_origins' => ['http://localhost:3000','http://localhost:3001','https://kyara-aura-web.vercel.app'],
+    'allowed_origins' => ['*'],
+    
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
